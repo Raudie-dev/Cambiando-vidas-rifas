1c425e3a16c9c6b0f8577a3e75ac389c4421c2ce
JuanDiego3030 <juandiegoaranaperez@gmail.com>
2025-10-23 19:17:02 -0400

    Correccion de bugs

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -133,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://04c3925fdca1.ngrok-free.app']
